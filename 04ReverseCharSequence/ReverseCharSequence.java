d5a92ac9443a8694a342584719c76f2d10b89a8d
fionaxc <fcai2@stuy.edu>
2017-11-24 21:10:06 -0500

    Updated reversechar thanksgiving

--- 04ReverseCharSequence/ReverseCharSequence.java
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -1,5 +1,5 @@
 public class ReverseCharSequence implements CharSequence{
-    private bkstring;
+    private String  bkstring;
     public ReverseCharSequence(String str){
 	String ans = "";
 	for(int i = str.length-1; i>=0;i--){
