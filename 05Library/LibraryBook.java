d65b4fe4dbb7a29133e946ef69ea25f31e544342
j <b@js-MacBook-Pro.fios-router.home>
2017-12-04 20:21:47 -0500

    made abstract methods, compareto, and tostring

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,12 +1,26 @@
 public abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
     private String callNumber;
+    
     public LibraryBook(String a, String t, String I, String call){
-	super(a,t,I,call);
-	callNumber = call;
+	   super(a,t,I,call);
+	   callNumber = call;
     }
 
-   
-
+    public String getCallNumber(){
+        return callNumber;
+    }
+    
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+    
+    public int compareTo(LibraryBook other){
+        return Integer.compare(this.callNumber, other.callNumber);
+    }
+    
+    public String toString(){
+        return super.toString()+" "+this.circulationStatus()+" "+callNumber
+    }
 
 
 }
