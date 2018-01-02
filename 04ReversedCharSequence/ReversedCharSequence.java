2179edb12f9e7587f95cccbfdc6b58c694c395b8
fionaxc <fcai2@stuy.edu>
2018-01-02 14:20:07 -0500

    changed name of file

---
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -0,0 +1,37 @@
+public class ReversedCharSequence implements CharSequence{
+    public String bkstring;
+    public String string;
+    public ReversedCharSequence(String str){
+        String ans = "";
+	    for(int i = str.length()-1; i>=0;i--){
+	        ans += str.substring(i,i+1);
+	    }
+	    bkstring = ans;
+        string = str;
+    }
+    
+    public char charAt(int index){
+	   return bkstring.charAt(index);
+    }
+
+    public int length(){
+	   return bkstring.length();
+    }
+
+    public CharSequence subSequence(int start, int end){
+        ReverseCharSequence r = new ReverseCharSequence(bkstring.substring(start,end));
+        return new ReverseCharSequence(r.bkstring);
+    }
+
+    public String toString(){
+	   return bkstring;
+    }
+
+    /*
+    public static void main(String[] args){
+        String s = "ABCDEF";
+        ReversedCharSequence r = new ReversedCharSequence(s);
+        System.out.println(r.subSequence(0,4));
+    }
+    */
+}
