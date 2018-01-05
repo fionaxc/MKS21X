3b4257146fa9a2414759bb8ffe173780bef81e79
Fiona Cai <fionacai@Kooldas-MacBook-Pro.local>
2018-01-05 13:58:28 -0500

    fixed random heading

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -12,15 +12,9 @@ public class Barcode implements Comparable<Barcode>{
         if(zip.length() != 5 || this.checkNum(zip)){
             throw new IllegalArgumentException();
         }
-       
-        this.zip = zip;     
 
-<<<<<<< HEAD
         this.zip = zip;
       }
-=======
-    }
->>>>>>> fb52034814ec6e0a11ff509844c1b3b41f757ec1
 
     public String getZip(){
         return zip;
@@ -33,37 +27,18 @@ public class Barcode implements Comparable<Barcode>{
 	    	String ans = "";
 	    	int sum = 0;
 	    	for(int i = 0; i < zip.length(); i++){
-	    	    String target = zip.substring(i,i+1);
-	    	    sum += Integer.parseInt(target);
-	    	    ans += getNumCode(Integer.parseInt(target));
+	    	    sum += Integer.parseInt(zip.substring(i,i+1));
+	    	    ans += getNumCode(Integer.parseInt(zip.substring(i,i+1)));
 	    	}
 	    	ans += getNumCode(sum % 10);
 	    	return "|"+ ans + "|";
     }
-<<<<<<< HEAD
 
     private static String getNumCode(int i){
         String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
         return codes[i];
     }
 
-
-=======
-    
-    public int getSum(String target){
-        int sum = 0;
-        for(int i = 0; i<target.length();i++){
-            sum += Integer.parseInt(target.zip.substring(i,i+1));
-        }
-        return sum;
-    }
-    
-    private static String getNumCode(int i){
-    		String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
-    		return codes[i];
-    }
-   
->>>>>>> fb52034814ec6e0a11ff509844c1b3b41f757ec1
     public boolean checkNum(String target){
         int sum = 0;
         for(int i = 0; i< target.length();i++){
@@ -74,11 +49,6 @@ public class Barcode implements Comparable<Barcode>{
         return sum == 0;
     }
 
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> fb52034814ec6e0a11ff509844c1b3b41f757ec1
     public static String toZip(String code){
 		    	if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(code.length() - 1) != '|'){
 		    	    throw new IllegalArgumentException();
@@ -108,13 +78,8 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public static String toCode(String zip){
-<<<<<<< HEAD
-    		Barcode code = new Barcode(zip);
-    		return code.getCode();
-=======
     		Barcode tmp = new Barcode(zip);
     		return tmp.getCode();
->>>>>>> fb52034814ec6e0a11ff509844c1b3b41f757ec1
     }
 
     public String toString(){
@@ -122,8 +87,7 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public int compareTo(Barcode b){
-<<<<<<< HEAD
-	return this.zipToInt() - b.zipToInt();
+	     return this.zipToInt() - b.zipToInt();
     }
 
     public boolean equals(Barcode b){
@@ -131,14 +95,3 @@ public class Barcode implements Comparable<Barcode>{
     }
 
 }
-=======
-	return zipToInt() - b.zipToInt();
-    }
-
-    public boolean equals(Barcode b){
-	return zipToInt() == b.zipToInt();
-    }
-
-}
-
->>>>>>> fb52034814ec6e0a11ff509844c1b3b41f757ec1
