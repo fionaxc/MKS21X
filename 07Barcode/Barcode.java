217c265a350d969199cb56d1c8f792c09abe29e0
Fiona Cai <fionacai@Kooldas-MacBook-Pro.local>
2018-01-05 13:41:10 -0500

    fixed mroe errors

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,60 +1,41 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Barcode implements Comparable<Barcode>{
-    //Field variables
-	private String zip;
-    
-	//Constructor
+    private String zip;
     public Barcode(String zip){
         if(zip.length() != 5 || this.checkNum(zip)){
-<<<<<<< HEAD
-            throw new IllegalArgumentException;
-=======
             throw new IllegalArgumentException();
->>>>>>> 76803bfefb06a215816e4f467daa723f85f2790a
-        }
-        else{
-             this.zip = zip;     
         }
 
-    }
-    
-    //Getter Methods
+        this.zip = zip;
+      }
+
     public String getZip(){
         return zip;
     }
-    
+    private int zipToInt(){
+	     return Integer.parseInt(zip);
+    }
+
     public String getCode(){
 	    	String ans = "";
-<<<<<<< HEAD
 	    	int sum = 0;
-	    	for (int i = 0; i < zip.length(); i++){
-	    		String target = zip.substring(i,i+1);
+	    	for(int i = 0; i < zip.length(); i++){
+	    	    String target = zip.substring(i,i+1);
 	    	    sum += Integer.parseInt(target);
 	    	    ans += getNumCode(Integer.parseInt(target));
 	    	}
-	    	ans += getNumCode(total % 10);
+	    	ans += getNumCode(sum % 10);
 	    	return "|"+ ans + "|";
     }
-    
-    //get the sum of the zip
-    public int getSum(String target){
-        int sum = 0;
-        for(int i = 0; i<target.length;i++){
-            sum += Integer.parseInt(target.zip.substring(i,i+1));
-        }
-        return sum;
-    }
-    
-    //Match number to code
+
     private static String getNumCode(int i){
-    		String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
-    		return code[i];
+        String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
+        return codes[i];
     }
-    
-    //check if all numbers of zip are numbers 
+
+
     public boolean checkNum(String target){
         int sum = 0;
         for(int i = 0; i< target.length();i++){
@@ -64,25 +45,8 @@ public class Barcode implements Comparable<Barcode>{
         }
         return sum == 0;
     }
-    
-    
-=======
-	    	for (int i = 0; i < zip.length(); i++){
-	    		String target = zip.substring(i,i+1);
-	    	    ans += getNumCode(Integer.parseInt(target));
-	    	}
-      		int sum = getSum(zip);
-	    	ans += getNumCode(sum % 10);
-	    	return "|"+ ans + "|";
-    }
-    
->>>>>>> 76803bfefb06a215816e4f467daa723f85f2790a
-    //Compare TO
-    public int compareTo(Barcode target){
-    	return Integer.parseInt(zip) - Integer.parseInt(target.zip);
-        }
-    
-    //code to zip
+
+
     public static String toZip(String code){
 		    	if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(code.length() - 1) != '|'){
 		    	    throw new IllegalArgumentException();
@@ -91,13 +55,13 @@ public class Barcode implements Comparable<Barcode>{
 		    	int total = 0;
 		    	String ans = "";
 		    	String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
-		    	ArrayList<String> a = new ArrayList<String>();
+		    	ArrayList<String> tmp = new ArrayList<String>();
 		    	for (int i = 0; i < 10; i++){
-		    	    a.add(codes[i]);
+		    	    tmp.add(codes[i]);
 		    	}
-		    	
+
 		    	for (int i = 0; i < 25; i++){
-		    	    int found = a.indexOf(code.substring(i, i + 5));
+		    	    int found = tmp.indexOf(code.substring(i, i + 5));
 		    	    if (found == -1){
 		    	    		throw new IllegalArgumentException();
 		    	    }
@@ -105,52 +69,27 @@ public class Barcode implements Comparable<Barcode>{
 		    	    ans += found;
 		    	    i += 4;
 		    	}
+          if (!(code.substring(25,30).equals(getNumCode(total % 10)))){
+        	    throw new IllegalArgumentException();
+        	}
 		    	return ans;
     }
-    
-    //zip to code
+
     public static String toCode(String zip){
-    		Barcode tmp = new Barcode(zip);
-    		return tmp.getCode();
+    		Barcode code = new Barcode(zip);
+    		return code.getCode();
     }
-<<<<<<< HEAD
+
     public String toString(){
-        
+	return getCode() + " (" + zip + ")";
     }
+
+    public int compareTo(Barcode b){
+	return this.zipToInt() - b.zipToInt();
+    }
+
+    public boolean equals(Barcode b){
+	return this.zipToInt() == b.zipToInt();
+    }
+
 }
-=======
-    
-    //To string 
-    public String toString(){
-    	return getCode() + " (" + zip + ")";
-    }
-    
-    //Supplementary Methods---------------------
-    //get the sum of the zip
-    public int getSum(String target){
-        int sum = 0;
-        for(int i = 0; i<target.length();i++){
-            sum += Integer.parseInt(target.substring(i,i+1));
-        }
-        return sum;
-    }
-    
-    //Match number to code
-    private static String getNumCode(int i){
-    		String[] codes = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:", "|:|::"};
-    		return codes[i];
-    }
-    
-    //check if all numbers of zip are numbers 
-    public boolean checkNum(String target){
-        int sum = 0;
-        for(int i = 0; i< target.length();i++){
-            if("012345689".indexOf(target.charAt(i)) == -1){
-                sum ++;
-            }
-        }
-        return sum == 0;
-    }
-    
-}
->>>>>>> 76803bfefb06a215816e4f467daa723f85f2790a
