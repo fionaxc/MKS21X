7507f23f4e77ac0fa525a7090b3eca1da4da74f5
Fiona Cai <fionacai@Kooldas-MBP.fios-router.home>
2018-01-05 22:18:46 -0500

    fixed minor get methods

--- 05Library/CirculatingBook.java
+++ 05Library/CirculatingBook.java
@@ -1,49 +1,54 @@
 public class CirculatingBook extends LibraryBook{
     private String currentHolder;
     private String dueDate;
-    
+
     public CirculatingBook(String a, String t, String I, String call){
         super(a,t,I,call);
         currentHolder = null;
         dueDate = null;
     }
-    
+
     public String getCurrentHolder(){
         return currentHolder;
     }
-    
+
     public String getDueDate(){
         return dueDate;
     }
-    
+
     public void setCurrentHolder(String holdername){
         currentHolder = holdername;
     }
-    
+
     public void setDueDate(String date){
         dueDate = date;
     }
-    
+
     public void checkout(String patron, String due){
         currentHolder = patron;
         dueDate = due;
     }
-    
+
     public void returned(){
         currentHolder = null;
         dueDate = null;
     }
-    
+
     public String circulationStatus(){
-        if(currentHolder != null && dueDate != null){
-            return "Current Holder: "+getCurrentHolder()+ " Due Date: "+dueDate;    
+        if(currentHolder != null){
+            return "Current Holder: "+currentHolder+ " Due Date: "+dueDate;
         }
         else{
             return "book available on shelves";
         }
     }
-    
+
     public String toString(){
-        return super.toString() + this.circulationStatus();
+      if (currentHolder != null){
+    			return super.toString() + " Current Holder:  " + currentHolder + " Due Date: " + dueDate;
+    		}
+
+    		return super.toString();
+
     }
 }
--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,6 +1,6 @@
 public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
     private String callNumber;
-    
+
     public LibraryBook(String a, String t, String I, String call){
 	   super(a,t,I);
 	   callNumber = call;
@@ -9,20 +9,20 @@ public abstract class LibraryBook extends Book implements Comparable<LibraryBook
     public String getCallNumber(){
         return callNumber;
     }
-    
-    abstract void checkout(String patron, String due);
-    abstract void returned();
-    abstract String circulationStatus();
-    
+
+    public abstract void checkout(String patron, String due);
+    public abstract void returned();
+    public abstract String circulationStatus();
+
     public int compareTo(LibraryBook other){
-        return this.callNumber.compareTo(other.callNumber);
+        return callNumber.compareTo(other.getCallNumber());
     }
-    
+
     public void setCallNumber(String call){
         callNumber = call;
     }
     public String toString(){
-        return super.toString()+" Circulation Status:  "+this.circulationStatus()+" Call Number: "+callNumber;
+        return super.toString()+" Circulation Status:  "+circulationStatus()+" Call Number: "+callNumber;
     }
 
 
